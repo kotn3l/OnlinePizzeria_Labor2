17a7b9f48433e9aec25f24b9f5eb24ec96ec595f
LR <lehorichard@gmail.com>
2020-02-22 13:42:17 +0100

    Backend: Add Spring framework

---
+++ server/src/main/java/hu/onlinepizzeria/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package hu.onlinepizzeria.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ServerApplication {
+
+    @RequestMapping("/")
+    public static String Home(){
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
