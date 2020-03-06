63e1237dd8378df750701ae05ef94fa7be054ae6
LR <lehorichard@gmail.com>
2020-03-06 19:22:29 +0100

    backend: only show available pizza

--- Backend/server/src/main/java/hu/onlinepizzeria/server/pizzaService/pizzaRepo.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/pizzaService/pizzaRepo.java
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface pizzaRepo extends CrudRepository<Pizza, Integer> {
-    @Query(value="SELECT * FROM pizza WHERE discount_percent > 0", nativeQuery = true)
+    @Query(value="SELECT * FROM pizza WHERE discount_percent > 0 and unavailable = false", nativeQuery = true)
     List<Pizza> getDiscountedPizza();
 
+    @Query(value = "SELECT * FROM pizza where unavailable = false", nativeQuery = true)
+    List<Pizza> getVisiblePizza();
 }
