17dd1798e494c13f6fd19443f265e03a7e6ec002
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-03-06 17:43:52 +0100

    pizzaRepo.java - added getDiscountedPizza method

--- Backend/server/src/main/java/hu/onlinepizzeria/server/pizzaService/pizzaRepo.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/pizzaService/pizzaRepo.java
@@ -1,7 +1,12 @@
 package hu.onlinepizzeria.server.pizzaService;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface pizzaRepo extends CrudRepository<Pizza, Integer> {
+    @Query(value="SELECT * FROM pizza WHERE discount_percent > 0", nativeQuery = true)
+    List<Pizza> getDiscountedPizza();
 
 }
