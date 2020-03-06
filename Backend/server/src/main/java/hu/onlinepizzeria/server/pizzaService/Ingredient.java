1a1c97f85327e141f62e54f5141c43e8ce6ac2fc
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-03-06 19:48:26 +0100

    Ingredient.java - added ingredient

---
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/pizzaService/Ingredient.java
@@ -0,0 +1,36 @@
+package hu.onlinepizzeria.server.pizzaService;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name = "ingredient")
+public class Ingredient implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @OneToMany(mappedBy = "ingredients")
+    private Set<Pizza> pizzas;
+}
