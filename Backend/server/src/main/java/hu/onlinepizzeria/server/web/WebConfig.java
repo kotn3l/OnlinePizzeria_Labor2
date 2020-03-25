bc7ab74afe0addd1d088e0411eeb16b43a7124ad
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-03-25 23:41:53 +0100

    Add WebConfig #13

---
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/web/WebConfig.java
@@ -0,0 +1,17 @@
+package hu.onlinepizzeria.server.web;
+
+import hu.onlinepizzeria.server.core.service.PizzaManagerInterface;
+import hu.onlinepizzeria.server.dao.PizzaRepo;
+import hu.onlinepizzeria.server.service.PizzaManager;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class WebConfig {
+
+    @Bean
+    public PizzaManager pizzaManager(PizzaRepo repository){
+        return new PizzaManager(repository);
+    }
+
+}
