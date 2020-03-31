991d147a97279c79419051bc918a9f03416188b3
Palanek Krisztián <pal.kriah@gmail.com>
2020-03-31 09:45:24 +0200

    Main navigation and landing page AB#{17}

--- Frontend/Online-Pizzeria-Client/src/app/app-routing.module.ts
+++ Frontend/Online-Pizzeria-Client/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserMainComponent } from './components/user/user-main/user-main.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { UserPizzaViewComponent } from './components/user/user-pizza-view/user-pizza-view.component';
+import { UserCartComponent } from './components/user/user-cart/user-cart.component';
 
-const routes: Routes = [];
+
+import { AdminMainComponent } from './components/admin/admin-main/admin-main.component';
+
+
+
+
+const routes: Routes = [
+  {
+    path: '', component: UserMainComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: UserHomeComponent },
+      { path: 'pizzas', component: UserPizzaViewComponent },
+      { path: 'cart', component: UserCartComponent }
+    ]
+  },
+  {
+    path: 'admin', component: AdminMainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/Online-Pizzeria-Client/src/app/app.module.ts
+++ Frontend/Online-Pizzeria-Client/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserMainComponent } from './components/user/user-main/user-main.component';
+import { UserNavComponent } from './components/user/user-nav/user-nav.component';
+import { UserHomeComponent } from './components/user/user-home/user-home.component';
+import { UserPizzaViewComponent } from './components/user/user-pizza-view/user-pizza-view.component';
+import { UserCartComponent } from './components/user/user-cart/user-cart.component';
+import { AdminMainComponent } from './components/admin/admin-main/admin-main.component';
+import { PizzaItemComponent } from './components/user/pizza-item/pizza-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserMainComponent,
+    UserNavComponent,
+    UserHomeComponent,
+    UserPizzaViewComponent,
+    UserCartComponent,
+    AdminMainComponent,
+    PizzaItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
