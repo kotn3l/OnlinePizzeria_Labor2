f2fed501b3eaa296d5f44b8285ec390cf1d7eee4
LR <lehorichard@gmail.com>
2020-04-02 01:00:59 +0200

    User auth model + service + dao

--- Backend/server/src/main/java/hu/onlinepizzeria/server/config/SecurityConfiguration.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/config/SecurityConfiguration.java
@@ -1,17 +1,43 @@
 package hu.onlinepizzeria.server.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    DataSource dataSource;
+
+
+//    @Autowired
+//    JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(getPasswordEncoder());
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable().authorizeRequests()
+                /* .antMatchers("/admin").hasRole("ADMIN")
+                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")*/
+                .antMatchers("/api/login").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- Backend/server/src/main/java/hu/onlinepizzeria/server/controller/AuthenticationController.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/controller/AuthenticationController.java
@@ -1,9 +1,37 @@
 package hu.onlinepizzeria.server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.onlinepizzeria.server.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api")
 public class AuthenticationController {
+    @Autowired
+    AuthenticationService authenticationService;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @PostMapping("/login")
+    public @ResponseBody String loginUser(@RequestParam String email, @RequestParam String password) {
+        try {
+            System.out.println(email + password);
+            //UserDetails user = authenticationService.loadUserByLogin(email, password);
+            //System.out.println(user.getUsername()+ user.getPassword() + user.getAuthorities());
+            return String.valueOf(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password)));
+            //return "user.getPassword() + user.getAuthorities();"+ u.toString();
+            //UserDetails user = authenticationService.loadUserByLogin(email, password);
+            //return user.getPassword() + user.getAuthorities();
+        } catch (AuthenticationException e) {
+            //throw new BadCredentialsException("Invalid username/password");
+            throw e;
+        }
+
+        //User user = authenticationService.loadUserByLogin(email, password);
+
+
+    }
+
 }
