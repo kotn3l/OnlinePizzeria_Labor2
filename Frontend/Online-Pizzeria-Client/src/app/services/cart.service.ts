0c5b48b31d32495f4be8af85c40133538624520c
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-05 00:30:07 +0200

    Service for shopping cart #19

---
+++ Frontend/Online-Pizzeria-Client/src/app/services/cart.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { CartItem } from '../models/CartItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartService {
+  constructor() { }
+
+}
