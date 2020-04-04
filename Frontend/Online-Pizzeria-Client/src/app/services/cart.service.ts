4be7ba379d1cf764762fc117f145899fa353deb2
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-05 00:33:09 +0200

    CRUD operations for cart items #19

--- Frontend/Online-Pizzeria-Client/src/app/services/cart.service.ts
+++ Frontend/Online-Pizzeria-Client/src/app/services/cart.service.ts
@@ -32,4 +32,66 @@ export class CartService {
     });
     return sum;
   }
+
+  getCartItems(): Observable<CartItem[]> {
+    return of(this.cart);
+  }
+
+  addCartItem(cartItem: CartItem) {
+    if(this.cartCountSource.value + 1 > 15) {
+      return;
+    }
+
+    var added = false;
+    this.cart.forEach((item, index, cart) => {
+      if (item.pizzaId == cartItem.pizzaId) {
+        cart[index].count += cartItem.count;
+        added = true;
+      }
+    });
+
+    if (!added) {
+      this.cart.push(cartItem);
+    }
+
+    this.cartCountSource.next(this.getCartCount());
+    sessionStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  changeCartItemCount(pizzaId: number, count: number) {
+    if(this.cartCountSource.value + count > 15) {
+      return;
+    }
+
+    this.cart.forEach((item, index, cart) => {
+      if (item.pizzaId == pizzaId) {
+        cart[index].count += count;
+
+        if(cart[index].count < 1) {
+          this.deleteCartItem(pizzaId);
+        }
+      }
+    });
+
+    this.cartCountSource.next(this.getCartCount());
+    sessionStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  deleteCartItem(pizzaId: number) {
+    this.cart.forEach((item, index, cart) => {
+      if (item.pizzaId == pizzaId) {
+        cart.splice(index, 1);
+      }
+    });
+
+    this.cartCountSource.next(this.getCartCount());
+    sessionStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  clearCart() {
+    this.cart = [];
+    this.cartCountSource.next(this.getCartCount());
+
+    sessionStorage.removeItem('cart');
+  }
 }
