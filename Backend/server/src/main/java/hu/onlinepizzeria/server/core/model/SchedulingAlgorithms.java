d135e5ff22b20d15cbb6b1911c557126ed4aff27
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-04-04 22:55:05 +0200

    Add SchedulingAlgorithms.java model

---
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/core/model/SchedulingAlgorithms.java
@@ -0,0 +1,40 @@
+package hu.onlinepizzeria.server.core.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "scheduling")
+public class SchedulingAlgorithms {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    private String type;
+
+    private boolean is_active;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+}
