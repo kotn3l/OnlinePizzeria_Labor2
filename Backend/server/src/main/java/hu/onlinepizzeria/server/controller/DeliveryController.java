eefa914e9859864b5b33bcff94efe913d02d2654
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-04-04 22:45:59 +0200

    DeliveryController.java - set session_string and other parameters mandatory

--- Backend/server/src/main/java/hu/onlinepizzeria/server/controller/DeliveryController.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/controller/DeliveryController.java
@@ -9,25 +9,25 @@ public class DeliveryController {
     //private DeliveryManager deliveryManager;
     //constructor that sets the deliveryManager prop
 
-    @GetMapping(path="/delivery-guy/{session_string}")
+    @GetMapping(path="/delivery-guy/")
     public @ResponseBody
-    String getDeliveryGuys(@PathVariable String session_string){
+    String getDeliveryGuys(@RequestParam(name="session_string", required = true) String session_string){
         return "getDeliveryGuys not yet implemented";
     }
 
-    @PostMapping(path="/delivery/{session_string}/{delivery_guy}", headers = "")
-    public @ResponseBody String assignOrdersToDeliveryGuy(@PathVariable String session_string, @PathVariable Integer delivery_guy){
+    @PostMapping(path="/delivery/", headers = "")
+    public @ResponseBody String assignOrdersToDeliveryGuy(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="delivery_guy", required = true) Integer delivery_guy){
         return "assignOrdersToDeliveryGuy not yet implemented";
     }
 
-    @GetMapping(path="/delivery/{session_string}/{delivery_guy}")
+    @GetMapping(path="/delivery/")
     public @ResponseBody
-    String getDeliveriesOfAGuy(@PathVariable String session_string, @PathVariable Integer delivery_guy){
+    String getDeliveriesOfAGuy(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="delivery_guy", required = true) Integer delivery_guy){
         return "getDeliveriesOfAGuy not yet implemented";
     }
 
-    @PostMapping(path="/delivery/{session_string}/{delivery_id}")
-    public @ResponseBody String setOrderDelivered(@PathVariable String session_string, @PathVariable Integer delivery_id){
+    @PostMapping(path="/delivery/")
+    public @ResponseBody String setOrderDelivered(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="delivery_id", required = true) Integer delivery_id){
         return "setOrderDelivered not yet implemented";
     }
 }
