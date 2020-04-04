88de046b76477d9f7d63ad1df38965c56c0f6698
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-05 00:31:09 +0200

    Load items from session #19

--- Frontend/Online-Pizzeria-Client/src/app/services/cart.service.ts
+++ Frontend/Online-Pizzeria-Client/src/app/services/cart.service.ts
@@ -7,6 +7,29 @@ import { CartItem } from '../models/CartItem';
 })
 
 export class CartService {
+  cart: CartItem[] = this.loadCartItems();
+
+  private cartCountSource = new BehaviorSubject<number>(this.getCartCount());
+  cartCount = this.cartCountSource.asObservable();
+
+  maxOrder: number = 15;
+
   constructor() { }
 
+  private loadCartItems() {
+    if (sessionStorage.getItem('cart') == null) {
+      return [];
+    }
+    else {
+      return JSON.parse(sessionStorage.getItem('cart'));
+    }
+  }
+
+  private getCartCount(): number {
+    var sum = 0;
+    this.cart.forEach(item => {
+      sum += item.count;
+    });
+    return sum;
+  }
 }
