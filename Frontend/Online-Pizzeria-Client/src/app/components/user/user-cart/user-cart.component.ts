2b0ffda2cbe3db599820bc7111bffa0b4da809ff
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-05 16:36:14 +0200

    Main cart component reorganise #19

--- Frontend/Online-Pizzeria-Client/src/app/components/user/user-cart/user-cart.component.ts
+++ Frontend/Online-Pizzeria-Client/src/app/components/user/user-cart/user-cart.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../../../services/cart.service';
-import { CartItem } from '../../../models/CartItem';
 
 @Component({
   selector: 'app-user-cart',
@@ -9,27 +7,8 @@ import { CartItem } from '../../../models/CartItem';
 })
 export class UserCartComponent implements OnInit {
 
-  cartItems: CartItem[];
-  cartCount: number;
-
-  constructor(private cartService: CartService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.cartService.cartCount.subscribe(count => this.cartCount = count);
-    this.cartService.getCartItems().subscribe(cartItems => this.cartItems = cartItems);
-  }
-
-  getTotalCost() {
-    var sum = 0;
-    this.cartItems.forEach(item => sum += item.count * item.price);
-    return sum;
-  }
-
-  changeItemCount(pizzaId: number, number: number) {
-    this.cartService.changeCartItemCount(pizzaId, number);
-  }
-
-  deleteItem(pizzaId: number) {
-    this.cartService.deleteCartItem(pizzaId);
   }
 }
