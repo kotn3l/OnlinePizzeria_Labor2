d5b64c081faab5fa71a3aaa6bb79fa4f7ab564e2
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-06 06:45:27 +0200

    Modell for order #19

---
+++ Frontend/Online-Pizzeria-Client/src/app/models/Order.ts
@@ -0,0 +1,12 @@
+export interface Order {
+    name: string;
+    email: string;
+    telephone: string;
+    city: string;
+    street: string;
+    house_number: string;
+    other: string;
+    comment: string;
+    pay_method: string;
+    order: number[];
+}
