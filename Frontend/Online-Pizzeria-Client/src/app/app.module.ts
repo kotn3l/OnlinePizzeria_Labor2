738e239714293181a7fece3cb27d3ef4debc56e3
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-06 06:49:49 +0200

    Adding form module to app #19

--- Frontend/Online-Pizzeria-Client/src/app/app.module.ts
+++ Frontend/Online-Pizzeria-Client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -36,7 +37,8 @@ import { UserCartFormComponent } from './components/user/user-cart-form/user-car
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
