a6d6ac25baa1701797284a041a8079e683156d7e
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-06 06:50:01 +0200

    Order form validation and post #19

--- Frontend/Online-Pizzeria-Client/src/app/components/user/user-cart-form/user-cart-form.component.ts
+++ Frontend/Online-Pizzeria-Client/src/app/components/user/user-cart-form/user-cart-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { FormDropdown } from '../../../models/FormDropdown';
+import { Order } from '../../../models/Order';
+import { UtilityService } from '../../../services/utility.service';
+import { CartService } from '../../../services/cart.service';
+import { OrderService } from '../../../services/order.service';
 
 @Component({
   selector: 'app-user-cart-form',
@@ -7,30 +13,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCartFormComponent implements OnInit {
 
-  payMethods: [
-    {
-      "id": number,
-      "method": string
-    },
-    {
-      "id": number,
-      "method": string
-    }
-  ]
+  payMethods: FormDropdown[];
+  cities: FormDropdown[];
 
-  constructor() { }
+  currentOrder: Order = {
+    name: '',
+    email: '',
+    telephone: '',
+    city: '1',
+    street: '',
+    house_number: '',
+    other: '',
+    comment: '',
+    pay_method: '',
+    order: []
+  }
+
+  orderPosted: boolean = false;
+  orderSucces: boolean = false;
+  orderFail: boolean = false;
+  responseMessage: string[] = [];
+
+  @ViewChild('orderForm', { static: false }) form: any;
+
+  constructor(
+    private utilityService: UtilityService,
+    private cartService: CartService,
+    private orderService: OrderService
+  ) { }
 
   ngOnInit() {
-    this.payMethods = [
-      {
-        "id": 1,
-        "method": "cash"
-      },
-      {
-        "id": 2,
-        "method": "bank card"
-      }
-    ]
+    this.utilityService.getPayMethods().subscribe(methods => this.payMethods = methods);
+    this.utilityService.getCities().subscribe(cities => this.cities = cities);
+  }
+
+  onSubmit({ value, valid }: { value: Order, valid: boolean }) {
+    if (!valid) {
+      console.log('Form is not valid');
+    } else {
+      value.order = this.cartService.getItemIdsForOrder();
+
+      this.orderService.postOrder(value)
+        .subscribe(
+          next => {
+            this.orderSucces = true;
+            this.orderFail = false;
+
+            this.cartService.clearCart();
+            this.form.reset();
+
+            this.currentOrder = {
+              name: '',
+              email: '',
+              telephone: '',
+              city: '1',
+              street: '',
+              house_number: '',
+              other: '',
+              comment: '',
+              pay_method: '',
+              order: []
+            }
+          },
+          error => {
+            this.orderFail = true;
+            this.orderSucces = false;
+
+            this.responseMessage = [];
+            for (var key in error.error) {
+              this.responseMessage.push(error.error[key]);
+            }
+          }
+        );
+      this.orderPosted = true;
+    }
   }
 
 }
