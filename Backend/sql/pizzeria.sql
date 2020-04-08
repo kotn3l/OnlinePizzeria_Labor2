85ec9eded0b6d5bb5df2436573b0430f83a9dc83
LR <lehorichard@gmail.com>
2020-04-08 07:30:39 +0200

    Backend: update DB user tables

--- Backend/sql/pizzeria.sql
+++ Backend/sql/pizzeria.sql
@@ -2,10 +2,10 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1:3308
--- Létrehozás ideje: 2020. Már 06. 12:26
--- Kiszolgáló verziója: 8.0.18
--- PHP verzió: 7.3.12
+-- Host: localhost:3306
+-- Generation Time: Apr 08, 2020 at 07:23 AM
+-- Server version: 10.4.12-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,61 +19,78 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `pizzeria`
+-- Database: `pizzeria`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `customer`
+-- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `telephone` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
+  `telephone` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `delivery_man_order`
+-- Table structure for table `delivery_man_order`
 --
 
-DROP TABLE IF EXISTS `delivery_man_order`;
-CREATE TABLE IF NOT EXISTS `delivery_man_order` (
+CREATE TABLE `delivery_man_order` (
   `order_id` int(11) NOT NULL,
   `delivery_man` int(11) NOT NULL,
-  `turn` int(11) NOT NULL,
-  KEY `order_id` (`order_id`),
-  KEY `delivery_man` (`delivery_man`)
+  `turn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `ingredient`
+-- Table structure for table `hibernate_sequence`
 --
 
-DROP TABLE IF EXISTS `ingredient`;
-CREATE TABLE IF NOT EXISTS `ingredient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(5);
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `order`
+-- Table structure for table `ingredient`
 --
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ingredient` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ingredient`
+--
+
+INSERT INTO `ingredient` (`id`, `name`) VALUES
+(1, 'ham'),
+(2, 'pineapple');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `city` varchar(255) NOT NULL,
   `street` varchar(255) NOT NULL,
@@ -83,162 +100,357 @@ CREATE TABLE IF NOT EXISTS `order` (
   `pay_method` int(11) NOT NULL,
   `deadline` datetime NOT NULL,
   `state` int(11) NOT NULL,
-  `delivered` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `customer_id` (`customer_id`),
-  KEY `pay_method` (`pay_method`)
+  `delivered` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `order_pizza`
+-- Table structure for table `order_pizza`
 --
 
-DROP TABLE IF EXISTS `order_pizza`;
-CREATE TABLE IF NOT EXISTS `order_pizza` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `order_pizza` (
+  `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `pizza_id` int(11) NOT NULL,
-  `done` bit(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `order_id` (`order_id`),
-  KEY `pizza_id` (`pizza_id`)
+  `done` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `pay_method`
+-- Table structure for table `pay_method`
 --
 
-DROP TABLE IF EXISTS `pay_method`;
-CREATE TABLE IF NOT EXISTS `pay_method` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `pay_method` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `pizza`
+-- Table structure for table `pizza`
 --
 
-DROP TABLE IF EXISTS `pizza`;
-CREATE TABLE IF NOT EXISTS `pizza` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pizza` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` int(11) NOT NULL,
   `picture_path` varchar(255) NOT NULL,
   `discount_percent` int(11) NOT NULL,
-  `unavailable` bit(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `unavailable` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- A tábla adatainak kiíratása `pizza`
+-- Dumping data for table `pizza`
 --
 
 INSERT INTO `pizza` (`id`, `name`, `price`, `picture_path`, `discount_percent`, `unavailable`) VALUES
-(1, 'nev', 1000, 'path', 25, b'0');
+(1, 'ananászos', 2006, 'path2', 0, b'0'),
+(2, 'ananászos', 2006, 'path2', 0, b'0'),
+(3, 'ananászos', 2006, 'path2', 0, b'0'),
+(4, '[Hawaii]', 1250, '[path5]', 0, b'0'),
+(43, 'magyaros', 1000, 'path23', 10, b'0');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `pizza_ingredients`
+-- Table structure for table `pizza_ingredients`
 --
 
-DROP TABLE IF EXISTS `pizza_ingredients`;
-CREATE TABLE IF NOT EXISTS `pizza_ingredients` (
+CREATE TABLE `pizza_ingredients` (
   `pizza_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL,
-  KEY `pizza_id` (`pizza_id`),
-  KEY `ingredient_id` (`ingredient_id`)
+  `ingredient_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pizza_ingredients`
+--
+
+INSERT INTO `pizza_ingredients` (`pizza_id`, `ingredient_id`) VALUES
+(4, 1),
+(4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'ROLE_USER'),
+(2, 'ROLE_ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scheduled_pizzas`
+--
+
+CREATE TABLE `scheduled_pizzas` (
+  `order_pizza` int(11) NOT NULL,
+  `prep_number` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `role`
+-- Table structure for table `scheduling`
 --
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `scheduled_pizzas`
---
-
-DROP TABLE IF EXISTS `scheduled_pizzas`;
-CREATE TABLE IF NOT EXISTS `scheduled_pizzas` (
-  `order_pizza` int(11) NOT NULL AUTO_INCREMENT,
-  `prep_number` int(11) NOT NULL,
-  PRIMARY KEY (`order_pizza`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `scheduling`
---
-
-DROP TABLE IF EXISTS `scheduling`;
-CREATE TABLE IF NOT EXISTS `scheduling` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `scheduling` (
+  `id` int(11) NOT NULL,
   `type` varchar(100) NOT NULL,
-  `is_active` bit(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `is_active` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `user_role`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE IF NOT EXISTS `user_role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `role` int(11) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `role` (`role`)
+  `password` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Megkötések a kiírt táblákhoz
+-- Dumping data for table `user`
 --
 
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Teszt Elek', 'teszt.elek@example.com', '$2y$12$1C07W9ytuB/ksTPVRZ001.dM6LIlGcT.U.TzGlyQFMut81ReMq7y2'),
+(2, 'Gipsz Jakab', 'gipsum@citromail.hu', '$2y$12$0xnYRD0YcSvyq9DX//cNX.a6BpVVzcrhqlvXhOF1Ug2JS6SZHl81m'),
+(3, 'Jacob Gypsum', 'test1@gmail.com', '$2a$10$PpHKZSBfPBZEUWr66TjInugfp/Hasy1cva/UTj.d.D8K6HQgKNFx6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_id` int(10) NOT NULL,
+  `roles` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_id`, `roles`) VALUES
+(1, 'ROLE_USER'),
+(2, 'ROLE_USER'),
+(2, 'ROLE_ADMIN');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `delivery_man_order`
+--
 ALTER TABLE `delivery_man_order`
-  ADD CONSTRAINT `fk_delivery_man_order_delivery_man` FOREIGN KEY (`delivery_man`) REFERENCES `user_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `fk_delivery_man_order_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `delivery_man` (`delivery_man`);
 
+--
+-- Indexes for table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order`
+--
 ALTER TABLE `order`
-  ADD CONSTRAINT `fk_order_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `fk_order_pay_method` FOREIGN KEY (`pay_method`) REFERENCES `pay_method` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `pay_method` (`pay_method`);
 
+--
+-- Indexes for table `order_pizza`
+--
 ALTER TABLE `order_pizza`
-  ADD CONSTRAINT `fk_order_pizza_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `fk_order_pizza_pizza_id` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `pizza_id` (`pizza_id`);
 
+--
+-- Indexes for table `pay_method`
+--
+ALTER TABLE `pay_method`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pizza_ingredients`
+--
 ALTER TABLE `pizza_ingredients`
-  ADD CONSTRAINT `fk_pizza_ingredients_ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `fk_pizza_ingredients_pizza_id` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD KEY `pizza_id` (`pizza_id`),
+  ADD KEY `ingredient_id` (`ingredient_id`);
 
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `scheduled_pizzas`
+--
 ALTER TABLE `scheduled_pizzas`
-  ADD CONSTRAINT `fk_scheduled_pizzas_order_pizza` FOREIGN KEY (`order_pizza`) REFERENCES `order_pizza` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD PRIMARY KEY (`order_pizza`);
 
-ALTER TABLE `user_role`
-  ADD CONSTRAINT `fk_user_role_role` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+--
+-- Indexes for table `scheduling`
+--
+ALTER TABLE `scheduling`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD KEY `FK55itppkw3i07do3h7qoclqd4k` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ingredient`
+--
+ALTER TABLE `ingredient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_pizza`
+--
+ALTER TABLE `order_pizza`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pay_method`
+--
+ALTER TABLE `pay_method`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `scheduled_pizzas`
+--
+ALTER TABLE `scheduled_pizzas`
+  MODIFY `order_pizza` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `scheduling`
+--
+ALTER TABLE `scheduling`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `delivery_man_order`
+--
+ALTER TABLE `delivery_man_order`
+  ADD CONSTRAINT `fk_delivery_man_order_delivery_man` FOREIGN KEY (`delivery_man`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `fk_delivery_man_order_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_order_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `fk_order_pay_method` FOREIGN KEY (`pay_method`) REFERENCES `pay_method` (`id`);
+
+--
+-- Constraints for table `order_pizza`
+--
+ALTER TABLE `order_pizza`
+  ADD CONSTRAINT `fk_order_pizza_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`),
+  ADD CONSTRAINT `fk_order_pizza_pizza_id` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
+
+--
+-- Constraints for table `pizza_ingredients`
+--
+ALTER TABLE `pizza_ingredients`
+  ADD CONSTRAINT `fk_pizza_ingredients_ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`),
+  ADD CONSTRAINT `fk_pizza_ingredients_pizza_id` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
+
+--
+-- Constraints for table `scheduled_pizzas`
+--
+ALTER TABLE `scheduled_pizzas`
+  ADD CONSTRAINT `fk_scheduled_pizzas_order_pizza` FOREIGN KEY (`order_pizza`) REFERENCES `order_pizza` (`id`);
+
+--
+-- Constraints for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
