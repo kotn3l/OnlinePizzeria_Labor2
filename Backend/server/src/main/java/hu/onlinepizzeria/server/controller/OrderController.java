6aa37a51dad1ed4404a0aa8f8296d20651bb7156
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-04-16 17:21:59 +0200

    OrderController - use orderManager methods #14

--- Backend/server/src/main/java/hu/onlinepizzeria/server/controller/OrderController.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/controller/OrderController.java
@@ -1,7 +1,13 @@
 package hu.onlinepizzeria.server.controller;
 
+import hu.onlinepizzeria.server.core.model.DeliveryCities;
+import hu.onlinepizzeria.server.core.model.Order;
+import hu.onlinepizzeria.server.core.model.PayMethod;
+import hu.onlinepizzeria.server.core.model.Pizza;
 import hu.onlinepizzeria.server.service.OrderManager;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path="/api")
@@ -13,34 +19,33 @@ public class OrderController {
     }
 
     @PostMapping(path="/order")
-    public @ResponseBody
-    String newOrder (@RequestParam String name, @RequestParam String email, @RequestParam String telephone, @RequestParam String city, @RequestParam String street, String houseNum, String other, String comment, String payMethod){
-        return "newOrder not implemented " + " replace String at payMethod with PayMethod class if it's done";
+    public @ResponseBody String newOrder (@RequestBody Map<String, Object> order){
+        return orderManager.addNewOrder(order);
     }
 
     @GetMapping(path="/pay-method")
-    public @ResponseBody String getPayMethod(){
-        return "getPayMethod not yet implemented";
+    public @ResponseBody Iterable<PayMethod> getPayMethod(){
+        return orderManager.getPayMethods();
     }
 
     @GetMapping(path="/delivery-city")
-    public @ResponseBody String getDeliveryCities(){
-        return "getDeliveryCities not yet implemented";
+    public @ResponseBody Iterable<DeliveryCities> getDeliveryCities(){
+        return orderManager.getDeliveryCities();
     }
 
     @GetMapping(path="/order-prep/")
-    public @ResponseBody String getPrepOrder(@RequestParam(name="session_string", required = true) String session_string){
-        return "getPrepOrder not yet implemented";
+    public @ResponseBody Iterable<Pizza> getPrepOrder(@RequestParam(name="session_string", required = true) String session_string){
+        return orderManager.getPreparedPizzas();
     }
 
     @PostMapping(path="/order-prep/")
     public @ResponseBody String setPizzaPrepared(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="ordered_pizza_id", required = true) Integer ordered_pizza_id){
-        return "setPizzaPrepared not yet implemented";
+        return orderManager.pizzaPrepared(ordered_pizza_id);
     }
 
     @GetMapping(path="/order-delivery/")
-    public @ResponseBody String getReadyOrders(@RequestParam(name="session_string", required = true) String session_string){
-        return "getReadyOrders not yet implemented";
+    public @ResponseBody Iterable<Order> getReadyOrders(@RequestParam(name="session_string", required = true) String session_string){
+        return orderManager.getReadyOrders();
     }
 
 }
