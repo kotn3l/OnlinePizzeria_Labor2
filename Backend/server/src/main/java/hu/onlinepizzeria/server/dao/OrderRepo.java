a1f224b1317ae0eb3c4de6e9acd449de907352dc
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-04-19 20:12:51 +0200

    OrderRepo.java - change order to orders, also add new queries #14

--- Backend/server/src/main/java/hu/onlinepizzeria/server/dao/OrderRepo.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/dao/OrderRepo.java
@@ -13,20 +13,26 @@ import java.util.ArrayList;
 public interface OrderRepo extends CrudRepository<Order, Integer> {
     @Modifying
     @Transactional
-    @Query(value="INSERT INTO `order` (customer_id, city, street, house_number, other, comment, pay_method, deadline, state, delivered) " +
+    @Query(value="INSERT INTO orders (customer_id, city, street, house_number, other, comment, pay_method, deadline, state, delivered) " +
             "VALUES (:customer_id, :city, :street, :house_number, :other, :comment, :pay_method, :deadline, :state, :delivered)", nativeQuery = true)
     void addNewOrder(Integer customer_id, Integer city, String street, Integer house_number, String other, String comment, Integer pay_method, Timestamp deadline, Integer state, Timestamp delivered);
 
-    @Query(value="SELECT * FROM `order` WHERE id IN (SELECT order_id FROM order_pizza WHERE done = 1)", nativeQuery = true) //is this right?
-    ArrayList<Order> getReadyPizzasPerOrder();
+    @Query(value="SELECT * FROM orders WHERE id = :order_id", nativeQuery = true)
+    ArrayList<Order> getOrderById(Integer order_id);
 
     @Modifying
     @Transactional
-    @Query(value="UPDATE order_pizza SET done=1 WHERE pizza_id=:ordered_pizza_id", nativeQuery = true)
+    @Query(value="UPDATE order_pizza SET done=1 WHERE pizza_id=:ordered_pizza_id AND done=0 LIMIT 1", nativeQuery = true)
     void pizzaPrepared(Integer ordered_pizza_id);
 
-    @Query(value="SELECT pizza_id FROM order_pizza WHERE done = 1", nativeQuery = true)
-    ArrayList<Integer> preparedPizzas();
+    @Query(value="SELECT DISTINCT order_id FROM order_pizza WHERE done = 1", nativeQuery = true)
+    ArrayList<Integer> donePizzas();
+
+    @Query(value="SELECT DISTINCT order_id FROM order_pizza WHERE done = 0 AND order_id=:order_id", nativeQuery = true)
+    ArrayList<Integer> checkOrder(Integer order_id);
+
+    @Query(value="SELECT pizza_id FROM order_pizza", nativeQuery = true)
+    ArrayList<Integer> prepOrderPizzas();
 
     @Modifying
     @Transactional
