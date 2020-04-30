e05f1e97237864a8f2cc91254abff2fcc75c3611
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-30 13:49:02 +0200

    Models for authentication #26

---
+++ Frontend/Online-Pizzeria-Client/src/app/models/UserData.ts
@@ -0,0 +1,6 @@
+import { UserRole } from './UserRole';
+
+export class UserData {
+    session_string: string;
+    role: UserRole
+}
---
+++ Frontend/Online-Pizzeria-Client/src/app/models/UserRole.ts
@@ -0,0 +1,6 @@
+export enum UserRole {
+    administrator = 'administrator',
+    manager = 'manager',
+    deliveryGuy = 'delivery guy',
+    kitchenStaff = 'kitchen staff'
+}
