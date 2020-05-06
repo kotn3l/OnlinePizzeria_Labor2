dd351c19f6529d7a6ea979d55ca10836b34174ec
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-06 12:09:56 +0200

    Models for services #30

---
+++ Frontend/Online-Pizzeria-Client/src/app/models/DeliveryGuy.ts
@@ -0,0 +1,4 @@
+export class DeliveryGuy {
+    name: string;
+    id: number;
+}
---
+++ Frontend/Online-Pizzeria-Client/src/app/models/OrderForDelivery.ts
@@ -0,0 +1,8 @@
+export class OrderForDelivery {
+    order_id: number
+    city: string
+    street: string
+    house_number: string
+    other: string
+    pizza_count: number
+}
