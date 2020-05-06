baa4137a4a8fafdcc7a9b1c65682b404f962c46e
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-06 12:09:13 +0200

    Pizza service for admin pizza page #30

--- Frontend/Online-Pizzeria-Client/src/app/services/pizza.service.ts
+++ Frontend/Online-Pizzeria-Client/src/app/services/pizza.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 import { Pizza } from '../models/Pizza';
+import { AuthService } from './auth.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-type': 'application/json' })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+    ) { }
 
   getDiscountedPizzas(): Observable<Pizza[]> {
     return this.http.get<Pizza[]>(`${environment.apiBaseUrl}/api/pizzas/discount`);
@@ -18,5 +26,47 @@ export class PizzaService {
 
   getAllPizzas() {
     return this.http.get<Pizza[]>(`${environment.apiBaseUrl}/api/pizzas`);
+  }
+
+  addPizza(pizza: Pizza, pictureFile: File) {
+    var postPizza = {
+      name: pizza.name,
+      price: pizza.price,
+      ingredients: pizza.ingredients,
+      picture: pictureFile
+    }
+    console.log(postPizza);
+
+    return this.http.post(`${environment.apiBaseUrl}/api/pizza/?session_string=${this.authService.userData.session_string}`, postPizza, httpOptions);
+  }
+
+  updatePizza(pizza: Pizza, pictureFile: File) {
+    var postPizza = {
+      name: pizza.name,
+      price: pizza.price,
+      ingredients: pizza.ingredients,
+      picture: pictureFile == null ? pizza.picture : pictureFile,
+      discount_price: pizza.discount_price
+    }
+
+    return this.http.put(`${environment.apiBaseUrl}/api/pizza/?session_string=${this.authService.userData.session_string}&pizza_id=${pizza.id}`, postPizza, httpOptions);
+  }
+
+  deletePizza(id: number) {
+    return this.http.delete(`${environment.apiBaseUrl}/api/pizza/?session_string=${this.authService.userData.session_string}&pizza_id=${id}`);
+  }
+
+  private pizzaSource = new BehaviorSubject<Pizza>({ id: null, name: null, price: null, ingredients: null, picture: null, discount_price: null });
+  selectedPizza = this.pizzaSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  setFormPizza(pizza: Pizza) {
+    this.pizzaSource.next(pizza);
+  }
+
+  clearFormPizza() {
+    this.pizzaSource.next({ id: null, name: null, price: null, ingredients: null, picture: null, discount_price: null });
   }
 }
