77a3d4c639dc592b5bd89565f842276a6e1be4a1
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-06 12:08:36 +0200

    Order service for make delivery page #30

--- Frontend/Online-Pizzeria-Client/src/app/services/order.service.ts
+++ Frontend/Online-Pizzeria-Client/src/app/services/order.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { Order } from '../models/Order';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { OrderForDelivery } from '../models/OrderForDelivery';
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-type': 'application/json' })
 }
 
 @Injectable({
@@ -12,9 +14,16 @@ const httpOptions = {
 })
 export class OrderService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
 
   postOrder(order: Order) {
     return this.http.post(`${environment.apiBaseUrl}/api/order`, order, httpOptions);
+  }
+
+  getReadyOrders() {
+    return this.http.get<OrderForDelivery[]>(`${environment.apiBaseUrl}/api/order-delivery?session_string=${this.authService.userData.session_string}`);
   }
 }
