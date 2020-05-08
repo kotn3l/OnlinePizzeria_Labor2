ec187b1b09f7cf709c3a7ced5c0ea05a9900bce5
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-09 00:09:41 +0200

    Order service for Kitchen staff #30

--- Frontend/Online-Pizzeria-Client/src/app/services/order.service.ts
+++ Frontend/Online-Pizzeria-Client/src/app/services/order.service.ts
@@ -4,6 +4,7 @@ import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { OrderForDelivery } from '../models/OrderForDelivery';
+import { OrderedPizza } from '../models/OrderedPizza';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-type': 'application/json' })
@@ -24,6 +25,14 @@ export class OrderService {
   }
 
   getReadyOrders() {
-    return this.http.get<OrderForDelivery[]>(`${environment.apiBaseUrl}/api/order-delivery?session_string=${this.authService.userData.session_string}`);
+    return this.http.get<OrderForDelivery[]>(`${environment.apiBaseUrl}/api/order-delivery/?session_string=${this.authService.userData.session_string}`);
+  }
+
+  getPrepareOrder() {
+    return this.http.get<OrderedPizza[]>(`${environment.apiBaseUrl}/api/order-prep/?session_string=${this.authService.userData.session_string}`);
+  }
+
+  postPizzaPrepared(id: number) {
+    return this.http.post(`${environment.apiBaseUrl}/api/order-prep/?session_string=${this.authService.userData.session_string}&orderd_pizza_id=${id}`, null);
   }
 }
