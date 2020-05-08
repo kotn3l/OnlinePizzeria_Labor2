99993858a4f62552d19f817094578b2e93b44ac8
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-09 00:11:51 +0200

    Pizza prepare page implementation #29

--- Frontend/Online-Pizzeria-Client/src/app/components/admin/admin-prepare-order/admin-prepare-order.component.ts
+++ Frontend/Online-Pizzeria-Client/src/app/components/admin/admin-prepare-order/admin-prepare-order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderedPizza } from 'src/app/models/OrderedPizza';
+import { OrderService } from 'src/app/services/order.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-admin-prepare-order',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-prepare-order.component.css']
 })
 export class AdminPrepareOrderComponent implements OnInit {
-
-  constructor() { }
+  orderedPizzas: OrderedPizza[];
+  constructor(
+    private orderService: OrderService,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    this.orderService.getPrepareOrder().subscribe(pizzas => {
+      this.orderedPizzas = pizzas;
+      this.orderedPizzas.sort((a, b) => { return a.number - b.number })
+    });
   }
 
+  ingredients(ingredients: string[]) {
+    var string = ingredients[0];
+    for (let i = 1; i < ingredients.length; i++) {
+      string = string.concat(', ' + ingredients[i]);
+    }
+    return string;
+  }
+
+  onDone(id: number) {
+    this.orderService.postPizzaPrepared(id).subscribe(() => {
+      this.orderService.getPrepareOrder().subscribe(pizzas => {
+        this.orderedPizzas = pizzas;
+        this.orderedPizzas.sort((a, b) => { return a.number - b.number })
+      });
+    },
+      err => this.flashMessage.show(err.error.error, { cssClass: 'alert-danger', timeout: 4000 }));
+  }
 }
