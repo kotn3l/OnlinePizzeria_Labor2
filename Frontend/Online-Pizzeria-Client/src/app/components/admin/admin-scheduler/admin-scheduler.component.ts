54f0edb74189febb9cf76f3c1e8f279c6501d49b
Palanek Krisztián <pal.kriah@gmail.com>
2020-05-09 00:12:07 +0200

    Scheduler set page implementation #29

--- Frontend/Online-Pizzeria-Client/src/app/components/admin/admin-scheduler/admin-scheduler.component.ts
+++ Frontend/Online-Pizzeria-Client/src/app/components/admin/admin-scheduler/admin-scheduler.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Scheduler } from 'src/app/models/Scheduler';
+import { SchedulingService } from 'src/app/services/scheduling.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-admin-scheduler',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-scheduler.component.css']
 })
 export class AdminSchedulerComponent implements OnInit {
-
-  constructor() { }
+  schedulers: Scheduler[];
+  current: number;
+  selected: number;
+  constructor(
+    private schedulingService: SchedulingService,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+    this.schedulingService.getSchedulingAlgorithms().subscribe(schedulers => {
+      this.schedulers = schedulers;
+      this.schedulers.forEach(scheduler => {
+        if (scheduler.is_active) {
+          this.current = scheduler.id;
+          this.selected = scheduler.id;
+        }
+      })
+    });
+  }
+
+  onSelect(id: number) {
+    this.selected = id;
+  }
+
+  onSubmit() {
+    this.schedulingService.postActiveAlgorithm(this.selected).subscribe(() => {
+      this.flashMessage.show('Scheduler changed', { cssClass: 'alert-success', timeout: 4000 });
+      this.current = this.selected;
+    },
+      err => {
+        this.flashMessage.show(err.error.error, { cssClass: 'alert-danger', timeout: 4000 });
+      }
+    );
   }
 
 }
