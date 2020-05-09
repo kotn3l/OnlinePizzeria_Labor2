c5c3abec6c52774c997958f390bd849aa44287e1
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-05-09 16:05:24 +0200

    SchedulingController - check session string where needed. Also added try-catches. #15

--- Backend/server/src/main/java/hu/onlinepizzeria/server/controller/SchedulingController.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/controller/SchedulingController.java
@@ -2,7 +2,13 @@ package hu.onlinepizzeria.server.controller;
 
 import hu.onlinepizzeria.server.core.model.SchedulingAlgorithms;
 import hu.onlinepizzeria.server.service.SchedulingManager;
+import hu.onlinepizzeria.server.service.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.InvalidParameterException;
 
 @RestController
 @RequestMapping(path="/api")
@@ -10,18 +16,41 @@ public class SchedulingController {
 
     private SchedulingManager schedulingManager;
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
     public SchedulingController(SchedulingManager schedulingManager) {
         this.schedulingManager = schedulingManager;
     }
 
     @GetMapping(path="/scheduling/")
-    public @ResponseBody
-    Iterable<SchedulingAlgorithms> getSchedulingAlgorithms(@RequestParam(name="session_string", required = true) String session_string){
-        return schedulingManager.getAlgorithms();
+    public @ResponseBody ResponseEntity getSchedulingAlgorithms(@RequestParam(name="session_string", required = true) String session_string){
+        try {
+            if (jwtTokenProvider.isAdmin(session_string)) { //TODO: manager role
+                return new ResponseEntity(schedulingManager.getAlgorithms(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+        }
+        catch (Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(path="/scheduling/")
-    public @ResponseBody String setActiveSchedulingAlgorithm(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="scheduling_id", required = true) Integer scheduling_id){
-        return schedulingManager.setActiveAlgorithm(scheduling_id);
+    public @ResponseBody ResponseEntity setActiveSchedulingAlgorithm(@RequestParam(name="session_string", required = true) String session_string, @RequestParam(name="scheduling_id", required = true) Integer scheduling_id){
+        try {
+            if (jwtTokenProvider.isAdmin(session_string)) { //TODO: manager role
+                return new ResponseEntity(schedulingManager.setActiveAlgorithm(scheduling_id), HttpStatus.OK);
+            } else {
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+        }
+        catch (InvalidParameterException ipe){
+            return new ResponseEntity(ipe.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        catch (Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
