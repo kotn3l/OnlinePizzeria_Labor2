2d7bc9b1ddfa8172fcf32e175b7fb64145f860c4
Mirkovszki Kornel <otiwir99@yahoo.com>
2020-05-11 17:47:54 +0200

    Add CorsConfig.java

---
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/web/CorsConfig.java
@@ -0,0 +1,16 @@
+package hu.onlinepizzeria.server.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("PUT", "DELETE", "GET", "POST").allowedOrigins("http://localhost:4200");
+    }
+}
