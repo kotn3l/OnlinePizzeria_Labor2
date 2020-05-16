50b75434c0cafc9ba979ba2b746a4bf2009689b0
LR <lehorichard@gmail.com>
2020-05-16 13:11:39 +0200

    Add User Builder

--- Backend/server/src/main/java/hu/onlinepizzeria/server/core/model/User.java
+++ Backend/server/src/main/java/hu/onlinepizzeria/server/core/model/User.java
@@ -3,7 +3,6 @@ package hu.onlinepizzeria.server.core.model;
 import com.fasterxml.jackson.annotation.JsonView;
 import hu.onlinepizzeria.server.core.Views;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -21,10 +21,17 @@ import static java.util.stream.Collectors.toList;
 @Entity
 @Table(name="user")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements UserDetails {
+
+    public User(String email, String password, String name, List<String> roles) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.roles = roles;
+    }
+
     @Id
     @JsonView(Views.Public.class)
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -49,7 +56,6 @@ public class User implements UserDetails {
 
     @JsonView(Views.Public.class)
     @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
     //@JoinColumn(name = "role", nullable = false)
     private List<String> roles = new ArrayList<>();
 
@@ -86,5 +92,44 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public static class UserBuilder {
+        private int id;
+        private String name;
+        private String email;
+        private String password;
+        private List<String> roles;
+
+        public UserBuilder(String email) {
+            this.email = email;
+        }
+
+        public UserBuilder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setRoles(String... role) {
+            this.roles.addAll(Arrays.asList(role));
+            return this;
+        }
+
+        public UserBuilder setRoles(List<String> roles) {
+            this.roles = roles;
+            return this;
+        }
+
+        public User build() { return new User(this.id, this.email, this.password, this.name, this.roles); }
     }
 }
